3ece73ff5a6c19bdb989d11fad70abf0650612d9
Wenjing Yang <yangwenjing@buaa.edu.cn>
2015-07-28 18:21:14 +0800

    my introduction

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('form.jade' ,{title: 'hello', })
+  res.render('form')
 });
-
 module.exports = router;
